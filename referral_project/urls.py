df1395589911833d03eb1ebb15ff86e5176880e3
Mikhail <Mikhail1Solovev@gmail.com>
2024-10-30 21:21:23 +0500

    first commit

--- referral_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from users.views import home
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),  # Убедитесь, что у вас есть этот путь
-    path('', home, name='home'),  # Добавляем маршрут для корневого URL
-]
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, verification, profile, login_view
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('verification/', verification, name='verification'),
-    path('profile/<str:phone>/', profile, name='profile'),
-    path('login/', login_view, name='login'),  # Добавьте эту строку
-]
